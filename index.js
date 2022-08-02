316be3ce59ac48aa9d4d41a20d1703ac829bfdef
Lincoln <lincoln@taylorlivingco.com>
2022-08-02 17:12:50 +1000

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// need tyo define feet varible before .
+
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  
+  else if(2000 < feet && feet < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } 
+
+  else if(feet > 2500)
+  {return 'No can do.';}
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+// using Ternary Expression 
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.':'No go.';
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  switch (tips) {
+    case 'generous':
+      return "Thank you so much.";
+
+    case 'not as generous':
+      return "Thank you.";
+
+      default :
+      return "Bye.";
+  }
 }
